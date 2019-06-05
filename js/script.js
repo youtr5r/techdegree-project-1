7d98d7622fa9e7eb6e62d3b26e939bf6c0c2ca20
Tristan Young <tristan.young@verizon.com>
2019-06-05 11:59:37 -0500

    project updated

--- js/script.js
+++ js/script.js
@@ -1,67 +1,72 @@
 /******************************************
-Treehouse FSJS Techdegree:
+Tristan Young: Techdegree
 project 1 - A Random Quote Generator
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
-
-
 /*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
+  Create an array with 5 objects.
+  Add 4 properties to each object: quote, source, citation, year.
+  Make sure to separate objects with comma.
 ***/
-
-
-
-
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
-
-
-
-
-/***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
-
-
-
-
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
-***/
-
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
-
+//Each quote should have one object with four proeperties
 var quotes = [
   {
-    quotes: "Faith is taking the first step even when you can't see the whole staircase.",
-    source: "Martin Luther King Jr. ",
+    quote: "Faith is taking the first step even when you can't see the whole staircase.",
+    source: "Martin Luther King Jr.",
     citation: "www.goodreads.com",
-    year: 2019
+    year: '2019'
   },
-  
+  {
+    quote: "Change takes courage.",
+    source: "Alexandria Ocasio-Cortez",
+    citation: "www.brainyquote.com",
+    year: '2018'
+  },
+  {
+    quote: "If you just work on stuff that you like and you’re passionate about, you don’t have to have a master plan with how things will play out.",
+    source: "Mark Zuckerberg" ,
+    citation: "www.goodreads.com",
+    year: '2019'
+  },
+  {
+    quote: "People say nothing is impossible, But I do nothing everyday. ",
+    source: "A.A. Milne, Winnie-the-Pooh",
+    citation: "https://theweekendfox.com/9-quotes-from-christopher-robin-that-are-good-for-the-soul/",
+    year: '2018'
+  }
 ];
-console.log(quotes);
+
+
+
+/***
+  Declare function named  `getRandomQuote` function to:
+   - Create variable named quoteGenerator to store random number 
+   - return quotes array
+***/
+function getRandomQuote(array) {
+var quoteGenerator = Math.floor( Math.random() * (quotes.length));
+return quotes[quoteGenerator];
+}
+
+//function expression in var result
+function printQuote() {
+var message = "";
+var result = getRandomQuote(quotes);
+message = "<p class='quote'>" + result.quote +"</p>";
+message += "<p class='source'>" + result.source;
+message += "<span class='citation'>" + result.citation + "</span>";
+message += "<span class='year'>" + result.year + "</span>"
+message += "</p>";
+
+document.getElementById('quote-box').innerHTML = message;
+//quote-box is element ID located in the css file
+}
+
+
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+document.getElementById('startQuote').addEventListner("click", printQuote, false);
+
+
+
+
+
